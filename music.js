5baa8f92f9f0b1f023e0d6be1429fdd5e91cfb89
Tim Hui <timboh56@gmail.com>
2012-11-01 22:43:14 -0400

    added stuff

---
+++ music.js
@@ -0,0 +1,27 @@
+
+
+var map = (function($){	
+	
+	// this map module should gather top artists for a specific country, 
+	// parse all the relevant information into arrays.
+	var my = {};
+	
+	my.getTopArtistsbyCountry = function(country){
+		var url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + country + '&api_key=ddf0769305146dfa3b94043ef8e5cb8d&format=json';
+		var artists = [];
+		$.ajax({
+			type: "GET",
+			url: url,
+			success: function(r){
+				for(var i in r.topartists.artist ){
+					artists[i] = r.topartists.artist[i];					
+					}
+
+				}
+			}
+		);
+		return artists;	
+	}
+	return my; 
+	
+}(jQuery));

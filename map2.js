5a6f2df0f753da943dff8cb74dfa20d35aa989d3
Tim Hui <timboh56@gmail.com>
2012-11-04 05:04:14 -0500

    Added map functionality, finds top artists for each country EXCEPT usa (need to fix that bug

--- map2.js
+++
@@ -1,1 +0,0 @@
-map.getTopArtistsbyCountry("Spain");
--- music.js
+++ music.js
@@ -1,27 +1,33 @@
-
-
-var map = (function($){	
+var map = (function ($) {
+  'use strict';
+  // this map module should gather top artists for a specific country, 
+  // parse all the relevant information into an array.
+  var my = {};
+	   
+  my.getTopArtistsbyCountry = function(country){
+    var i;
+	var url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + country + '&api_key=ddf0769305146dfa3b94043ef8e5cb8d&format=json';
+	var artists = new Array();		
 	
-	// this map module should gather top artists for a specific country, 
-	// parse all the relevant information into an array.
-	var my = {};
+	$.ajax({
+	  type: "GET",
+	  url: url,
+	  success: function(r){
+		var c = 1;
+		$("#topartists").append("<ul>");
+		for (i in r.topartists.artist) {
+			$("#topartists").append("<li> " + c +". " + r.topartists.artist[i].name+", \n </li>");
+		    //artists[i] = r.topartists.artist[i];
+		    c++;
+		  }
+		$("#topartists").append("</ul>");
+		
+	  }
+	});
 	
-	my.getTopArtistsbyCountry = function(country){
-		var url = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=' + country + '&api_key=ddf0769305146dfa3b94043ef8e5cb8d&format=json';
-		var artists = [];
-		$.ajax({
-			type: "GET",
-			url: url,
-			success: function(r){
-				for(var i in r.topartists.artist ){
-					artists[i] = r.topartists.artist[i];					
-					}
+  }
 
-				}
-			}
-		);
-		return artists;	
-	}
-	return my; 
-	
+  return my; 
+
 }(jQuery));
+
